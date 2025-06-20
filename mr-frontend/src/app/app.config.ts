a45538eb5508914af3723174f7c7efc7ba3460c6
da4de <damiir@gmail.com>
2025-06-20 13:23:49 +0200

    png compression, remove angular/router

--- mr-frontend/src/app/app.config.ts
+++ mr-frontend/src/app/app.config.ts
@@ -1,9 +1,7 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from "@angular/platform-browser/animations/async"
 import { providePrimeNG } from "primeng/config"
 import Aura from "@primeng/themes/aura"
-import { routes } from './app.routes';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { apiPrefixInterceptor } from '../http/api.prefix.interceptor';
 
@@ -14,7 +12,6 @@ export const appConfig: ApplicationConfig = {
       withInterceptors([apiPrefixInterceptor])
     ),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
     provideAnimationsAsync(),
     providePrimeNG({
       theme: {
