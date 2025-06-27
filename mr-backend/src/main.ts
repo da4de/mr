384c51c61b7e953711d0fab8a8954cf46b48ee59
da4de <damiir@gmail.com>
2025-06-27 17:54:52 +0200

    fix: CORS, setup: reverse proxy nginx

--- mr-backend/src/main.ts
+++ mr-backend/src/main.ts
@@ -4,8 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { WsAdapter } from '@nestjs/platform-ws';
 
 async function bootstrap() {
-  /* TODO setup CORS */
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(
     new ValidationPipe({ transform: true })
@@ -13,6 +12,6 @@ async function bootstrap() {
 
   app.useWebSocketAdapter(new WsAdapter(app));
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 bootstrap();
