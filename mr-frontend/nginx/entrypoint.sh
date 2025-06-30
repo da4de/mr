f25e697eebf1fd939884358bbc2f47ef0e35ed14
da4de <damiir@gmail.com>
2025-06-30 18:04:45 +0200

    fix: ssl name for deploy

--- mr-frontend/nginx/entrypoint.sh
+++ mr-frontend/nginx/entrypoint.sh
@@ -1,14 +1,20 @@
 #!/bin/sh
 : "${BACKEND_URL:=http://backend:3000/}"
+: "${BACKEND_HOST:=backend}"
 : "${BACKEND_WS_URL:=http://backend:3001}"
+: "${BACKEND_WS_HOST:=backend}"
 
 export BACKEND_URL
 export BACKEND_WS_URL
+export BACKEND_HOST
+export BACKEND_WS_HOST
 
 echo "BACKEND_URL = $BACKEND_URL"
 echo "BACKEND_WS_URL = $BACKEND_WS_URL"
+echo "BACKEND_HOST = $BACKEND_HOST"
+echo "BACKEND_WS_HOST = $BACKEND_WS_HOST"
 
-envsubst '${BACKEND_URL} ${BACKEND_WS_URL}' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
+envsubst '${BACKEND_URL} ${BACKEND_WS_URL} ${BACKEND_HOST} ${BACKEND_WS_HOST}' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
 
 exec "$@"
 
